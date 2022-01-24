2c04d57853c5b961deedb162d80d526c9d84bc74
周星星 <thuhak@163.com>
2022-01-24 21:42:00 +0800

    Update __init__.py
    auto load

--- software/__init__.py
+++ software/__init__.py
@@ -1,4 +1,13 @@
 """
 load all software parser
 """
+import importlib
+import os
 from .mainparser import MainParser
+
+utils = {'database', 'mainparser'}
+for module in os.listdir(os.path.dirname(__file__)):
+    if module.endswith('.py'):
+        name = os.path.basename(module).rsplit('.', maxsplit=1)[0]
+        if name not in utils:
+            importlib.import_module(f'.{name}', __package__)
