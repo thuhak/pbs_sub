6179e385a6024e34870924f5e8f04bf9ea0a5dab
周星星 <thuhak@163.com>
2022-01-24 21:50:54 +0800

    Update pbs_sub.py

--- pbs_sub.py
+++ pbs_sub.py
@@ -2,6 +2,5 @@
 from software import MainParser
 
 if __name__ == '__main__':
-    # MainParser.script_base = '/home/hpcsw/PBSsubmit/software/run_scripts'
     parser = MainParser()
     parser.run()
